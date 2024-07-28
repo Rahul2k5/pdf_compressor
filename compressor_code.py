6cd429707bc9131c16d4dfca34fed54ccdd5e1d4
Rahul2k5 <135107152+Rahul2k5@users.noreply.github.com>
2024-07-28 19:50:27 +0530

    Init Code

---
+++ compressor_code.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import zipfile
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+root = tk.Tk()
+root.geometry("350x100")
+root.title("ProjectGurukul Folder and File Compressor")
+
+
+def compress():
+   input_file = filedialog.askopenfilename()
+   output_file = filedialog.asksaveasfilename(defaultextension=".zip")
+   with zipfile.ZipFile(output_file, 'w') as jungle_zip:
+       jungle_zip.write(input_file, compress_type=zipfile.ZIP_DEFLATED)
+   messagebox.showinfo("ProjectGurukul(Congrats)", "Compressed file is saved!")
+
+
+
+
+frame1 = tk.Frame(root)
+frame1.pack(pady=20)
+
+
+
+
+l1 = tk.Label(frame1, text="SELECT:")
+l1.pack(side="left", padx=10)
+button1 = tk.Button(frame1, text="Compress your File", width=20, command=compress)
+button1.pack(side="right", padx=10)
+root.mainloop()
